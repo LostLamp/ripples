d52f2260d816ffdb8ebf7876e6873337ceecfce3
dz <13413684909@163.com>
2018-12-18 22:46:32 +0800

    清空仓库

--- ripples/ripples-web-ui/src/main/java/com/wave/ripples/web/ui/service/TbMemberLoginService.java
+++
@@ -1,7 +0,0 @@
-package com.wave.ripples.web.ui.service;
-
-import com.wave.ripples.web.ui.entity.TbMember;
-
-public interface TbMemberLoginService {
-    TbMember login(String loginId,String password);
-}
