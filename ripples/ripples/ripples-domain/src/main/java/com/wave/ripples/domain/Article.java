7a548606ab5fa762d0af68671b9cd59878e6cddb
dz <13413684909@163.com>
2018-12-19 09:20:14 +0800

    修改了ui模块的发布动态页面和后台

---
+++ ripples/ripples/ripples-domain/src/main/java/com/wave/ripples/domain/Article.java
@@ -0,0 +1,30 @@
+package com.wave.ripples.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 消息 对象实体类
+ * <p>@Title Article </p>
+ * <p>@Description TODO </p>
+ *
+ * @Author dz
+ * @Version 1.0.0
+ * @Date 2018/12/18 15:29
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Article implements Serializable {
+    private Long archive_id;
+    private Long member_id;
+    private String content;
+    private Long view_count;
+    private Date pub_time;
+    private Date updated;
+    private Integer good_num;
+    private Integer bad_num;
+
+}
